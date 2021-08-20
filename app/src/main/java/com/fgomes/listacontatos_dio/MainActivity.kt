f78af079f72b176bb8cf1854e7d97e377132bf3d
fernando gomes <fernando.gmsoli@gmail.com>
2021-08-20 18:12:00 -0300

    Created toolbar menu

--- app/src/main/java/com/fgomes/listacontatos_dio/MainActivity.kt
+++ app/src/main/java/com/fgomes/listacontatos_dio/MainActivity.kt
@@ -2,6 +2,10 @@ package com.fgomes.listacontatos_dio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -34,11 +38,37 @@ class MainActivity : AppCompatActivity() {
                     "(00) 0000-1111",
                     "img.png"
                 ),Contact(
-                    "Teteste teste",
+                    "Teste 2 teste",
                     "(00) 0000-1111",
                     "img.png"
                 )
             )
         )
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId){
+            R.id.item_menu_1 -> {
+                showToast("Menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
