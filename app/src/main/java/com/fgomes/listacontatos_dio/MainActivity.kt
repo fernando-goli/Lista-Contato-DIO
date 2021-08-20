7ee82299f7bc5d76523edd3ca06ea6f627a19420
fernando gomes <fernando.gmsoli@gmail.com>
2021-08-20 17:32:14 -0300

    Initial commit

--- app/src/main/java/com/fgomes/listacontatos_dio/MainActivity.kt
+++ app/src/main/java/com/fgomes/listacontatos_dio/MainActivity.kt
@@ -2,10 +2,43 @@ package com.fgomes.listacontatos_dio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Teteste teste",
+                    "(00) 0000-1111",
+                    "img.png"
+                ),Contact(
+                    "Teteste teste",
+                    "(00) 0000-1111",
+                    "img.png"
+                )
+            )
+        )
     }
 }
